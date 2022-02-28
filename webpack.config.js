749f91afd68a12783c23ef88cec49c27fd0b7b17
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-02-28 11:01:24 +0000

    added content to webpack and babel config files (minus scss)

--- webpack.config.js
+++ webpack.config.js
@@ -3,3 +3,24 @@ const path = require('path');
 function buildPath(relative) {
   return path.join(__dirname, relative);
 }
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: buildPath('public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    static: {
+      directory: buildPath('public')
+    }
+  }
+};
